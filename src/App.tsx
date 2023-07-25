ba1c24bfb063fb27698f64048028912c807d8da3
tyoung <taeyoung917@gmail.com>
2023-07-25 11:18:54 +0900

    axios study

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Todo extends TodoTitle {
+  id: number;
+}
+interface TodoTitle {
+  title: string;
 }
 
-export default App
+interface Edit {
+  targetId?: number | null;
+  content: string;
+}
+
+function App() {
+  const [todosLists, setTodosLists] = useState<Todo[] | null>(null);
+  const [todos, setTodos] = useState<TodoTitle>({
+    title: "",
+  });
+  const [edit, setEdit] = useState<Edit>({
+    targetId: null,
+    content: "",
+  });
+
+  const fetchTodo = async () => {
+    const { data } = await axios.get(`http://localhost:4001/todos`);
+    setTodosLists(data);
+  };
+
+  const onCreateTodos = async () => {
+    const { data } = await axios.post(`http://localhost:4001/todos`, todos);
+
+    setTodosLists((prev: Todo[] | null) =>
+      prev !== null ? [...prev, data] : [data]
+    );
+
+    // 또는
+    // fetchTodo();
+  };
+
+  const onClickDeleteButton = async (id: number) => {
+    axios.delete(`http://localhost:4001/todos/${id}`);
+    if (todosLists !== null) {
+      setTodosLists(todosLists?.filter((it) => it.id !== id));
+    }
+  };
+
+  useEffect(() => {
+    fetchTodo();
+  }, []);
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          margin: "1rem",
+        }}
+      >
+        <input
+          type="text"
+          placeholder="할 일 추가"
+          value={todos.title}
+          onChange={(e) => {
+            setTodos({ title: e.target.value });
+          }}
+        />
+        <button onClick={onCreateTodos}>추가</button>
+      </div>
+      <div>
+        {todosLists?.map((it) => (
+          <div key={it.id}>
+            {it.id} : {it.title}
+            <button
+              style={{
+                border: "1px solid red",
+                padding: ".2rem .4rem",
+                margin: ".5rem",
+              }}
+              onClick={() => onClickDeleteButton(it.id)}
+            >
+              삭제
+            </button>
+          </div>
+        ))}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          minWidth: "320px",
+        }}
+      >
+        <input
+          type="number"
+          style={{ width: "10%" }}
+          placeholder="숫자"
+          value={edit.targetId ?? ""}
+          onChange={(e) => {
+            setEdit({ ...edit, targetId: parseInt(e.target.value) });
+          }}
+        />
+        <input type="text" placeholder="변경할 텍스트" />
+        <button>수정</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
